571ad44c338d00917dce8014d934daf05d955637
MDowod1 <dawidbrych1007@gmail.com>
2025-09-06 17:40:15 +0200

    Delete manifest.js

--- manifest.js
+++
@@ -1,6 +0,0 @@
-{name: "", short_name: "", theme_color: "#101317", background_color: "#101317", display: "standalone"}
-background_color: "#101317"
-display: "standalone"
-name: ""
-short_name: ""
-theme_color: "#101317"
